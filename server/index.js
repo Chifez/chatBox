0110891cd05540c5db842efd41b920fdf8b1578e
emmanuel <chifez1@gmail.com>
2024-01-24 17:32:49 +0100

    changed the server origin

--- server/index.js
+++ server/index.js
@@ -71,7 +71,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173',
+    origin: 'https://chat-box-tawny.vercel.app/',
     methods: ['GET', 'POST'],
   },
 });

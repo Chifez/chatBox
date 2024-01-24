343e4a842642096b55abd50d78430ff853b9923b
emmanuel <chifez1@gmail.com>
2024-01-24 17:29:41 +0100

    server update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import Chats from './Chats';
 import Login from './Login';
 import useSocket from './useSocket';
 
-const socket = io('https://chat-box-server-ochre.vercel.app/');
+const socket = io('chatbox-production.up.railway.app');
 
 function App() {
   const {
